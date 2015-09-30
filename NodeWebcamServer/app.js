f384b1bc8c66424834be13af29e3307772ce36b3
chrisjgall <01tacoma>
2015-09-29 19:19:47 -0700

    bootstrap the site, add timelapse carousel

--- NodeWebcamServer/app.js
+++ NodeWebcamServer/app.js
@@ -138,6 +138,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 // development only
 if ('development' == app.get('env')) {
