110690e0b42848a23e1d0f818fb0811ce642f2a4
Chris Gall <chrisjgall2002@yahoo.com>
2015-10-02 12:16:13 -0700

    try to fix image not refreshing on refresh and renamed refresh to Get Latest in UI

--- NodeWebcamServer/routes/refresh.js
+++ NodeWebcamServer/routes/refresh.js
@@ -4,7 +4,7 @@ exports.get = function(req, res) {
     var settings = res.app.locals["settings"];
     if (settings.AllowRefreshNow) {
         capture.execute(settings);
-        res.redirect(307, '/');
+        res.redirect(200, '/');
     } else {
         res.send(401);
     }
