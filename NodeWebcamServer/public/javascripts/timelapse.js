ff5161251233278497f8437d466af6583ab94666
chrisjgall <01tacoma>
2015-10-11 08:37:59 -0700

    add progressbar to timelapse

---
+++ NodeWebcamServer/public/javascripts/timelapse.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#carousel-timelapse').on('slid.bs.carousel', function () {
+        var index = $('#carousel-timelapse .active').index('#carousel-timelapse .item');
+        var perc = Math.round(100* (index / totalImages));
+        $('.progress-bar').css('width', perc+'%').attr('aria-valuenow', perc);
+    });
+});
